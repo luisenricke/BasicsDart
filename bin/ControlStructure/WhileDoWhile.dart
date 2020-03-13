6d4dd9bfcf5f6ab62e6296773ae80b142b8af12f
Luis Villalobos <luisevm24@outlook.com>
2020-03-13 00:29:10 -0600

    learn: while and do while

---
+++ bin/ControlStructure/WhileDoWhile.dart
@@ -0,0 +1,13 @@
+void main() {
+  var count = 0;
+  do {
+    print('Talk to people');
+    count++;
+  } while (count < 5);
+
+  count = 0;
+  while (count < 5) {
+    print('Keep spending money');
+    count++;
+  }
+}
